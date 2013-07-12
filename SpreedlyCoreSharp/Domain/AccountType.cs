7a787fac700c59c2f28d83e7a389d0e57286a11c
Jon <lester@rdacorp.com>
2013-07-12 01:02:39 -0400

    added System.ComponentModel Description attributes to enums for pretty text

--- SpreedlyCoreSharp/Domain/AccountType.cs
+++ SpreedlyCoreSharp/Domain/AccountType.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Serialization;
+﻿using System.ComponentModel;
+using System.Xml.Serialization;
 
 namespace SpreedlyCoreSharp.Domain
 {
@@ -7,15 +8,18 @@ namespace SpreedlyCoreSharp.Domain
         [XmlEnum("")]
         None = 0,
 
+        [Description("Credit Card")]
         [XmlEnum("credit_card")]
         CreditCard,
 
         [XmlEnum("sprel")]
         SPREL,
 
+        [Description("Third Party")]
         [XmlEnum("third_party_token")]
         ThirdParty,
 
+        [Description("Bank Account")]
         [XmlEnum("bank_account")]
         BankAccount
     }
